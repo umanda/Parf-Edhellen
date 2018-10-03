2bf346c0763c2be9edab763f39cfb169b9d941c8
galadhremmin <zanathel@gmail.com>
2018-10-03 12:58:28 -0700

    Google Analytics for production

---
+++ src/resources/views/_shared/_ga.blade.php
@@ -0,0 +1,19 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26836717-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-26836717-1');
+window.addEventListener('ednavigate', function (ev) {
+gtag('config', 'UA-26836717-1', {'page_path':ev.detail.address});  
+gtag('event', 'page_view');
+});
+</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8268364504414566",
+    enable_page_level_ads: true
+  });
+</script>
